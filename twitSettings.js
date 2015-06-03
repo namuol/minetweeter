6117f17e892deb5435b3d66ad01a21133118ab37
Louis Acresti <louis.acresti@gmail.com>
2015-06-03 21:17:10 +0700

    use env for config

---
+++ twitSettings.js
@@ -0,0 +1,6 @@
+export default {
+  "consumer_key": process.env.TWIT_CONSUMER_KEY,
+  "consumer_secret": process.env.TWIT_CONSUMER_SECRET,
+  "access_token": process.env.TWIT_ACCESS_TOKEN,
+  "access_token_secret": process.env.TWIT_ACCESS_TOKEN_SECRET,
+};
